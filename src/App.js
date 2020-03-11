a970654e398e2fe1ef65c8527c09d89acb27ea34
lahavtalker <lahavtalker@gmail.com>
2020-03-11 13:35:58 +0200

    added empty map component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Map from './components/Map/Map';
 
 function App() {
   return (
     <div className="App">
-
+      <Map />
     </div>
   );
 }
