7a9a8e2205fd0abdc1c312b206afba5935f49d58
yahav mizrachi <yahav283@gmail.com>
2020-03-11 16:34:08 +0200

    add map beer sheva

--- src/components/Map/LeafletMap.js
+++ src/components/Map/LeafletMap.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import { Icon } from 'leaflet';
-import './LeafletMap.css';
+import React, { Component } from "react";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import "./LeafletMap.css";
+
+// const mapLink = '<a href="http://www.esri.com/">Esri</a>';
+// const wholink =
+//   "i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community";
+
+const position = [31.25181, 34.7913];
 
 class LeafletMap extends Component {
-    render() {
-        return (
-            <Map>
-
-            </Map>
-        );
-    }
+  render() {
+    return (
+      <Map center={position} zoom={13}>
+        <TileLayer
+          url="http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+          attribution='&copy;  <a href="http://www.esri.com/">Esri</a> '
+        />
+      </Map>
+    );
+  }
 }
 
 export default LeafletMap;
