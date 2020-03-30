40e3d31b9dc3e424f5b13be54cc1db5e70d3e2a3
lahavtalker <lahavtalker@gmail.com>
2020-03-30 13:13:05 +0300

    added test.json

--- src/components/Map/LeafletMap.js
+++ src/components/Map/LeafletMap.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
 import "./LeafletMap.css";
-import * as BuildingData from './BeerSheva.json';
+import * as data from './test.json';
 
 // const mapLink = '<a href="http://www.esri.com/">Esri</a>';
 // const wholink =
@@ -19,10 +19,11 @@ class LeafletMap extends Component {
           attribution='&copy;  <a href="http://www.esri.com/">Esri</a> '
           maxZoom={17.5}
         />
-        {BuildingData.features.map(building => (
-          <Marker key={building.properties.osm_id} position={[
-            building.geometry.coordinates[1],
-            building.geometry.coordinates[0]
+
+        {data.features.map(bld => (
+          <Marker key={bld.properties.osm_id} position={[
+            bld.geometry.coordinates[1],
+            bld.geometry.coordinates[0]
           ]} />
         ))}
       </Map>
