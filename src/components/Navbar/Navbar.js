6fc82a0205cf2298bba2117500e1788ad046e9a3
lahavtalker <lahavtalker@gmail.com>
2020-03-30 16:53:05 +0300

    fixed map

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,35 +3,35 @@ import "./Navbar.css";
 import Logo from "./Logo.png";
 
 class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar">
-          <div className="container">
-            <h1 className="logo">
-              <a href="#">
-                <span>S</span>olar <span>R</span>oofs <span>S</span>ervice
+	render() {
+		return (
+			<div>
+				<nav className="navbar">
+					<div className="container">
+						<h1 className="logo">
+							<a href="#">
+								<span>S</span>olar <span>R</span>oofs <span>S</span>ervice
                 <img className="website-logo" src={Logo} alt=""></img>
-              </a>
-            </h1>
-            <ul>
-              <li>
-                <a class="current" href="index.html">
-                  Private service
+							</a>
+						</h1>
+						<ul>
+							<li>
+								<a class="current" href="index.html">
+									Private service
                 </a>
-              </li>
-              <li>
-                <a href="about.html">Organizational service</a>
-              </li>
-              <li>
-                <a href="contact.html">Nationl service</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    );
-  }
+							</li>
+							<li>
+								<a href="about.html">Organizational service</a>
+							</li>
+							<li>
+								<a href="contact.html">Nationl service</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		);
+	}
 }
 
 export default Navbar;
